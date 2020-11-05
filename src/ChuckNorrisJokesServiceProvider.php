9a51f003628c4fb96340e8c1fae7092bbcb4d8cc
Freddy Munandar <freddymu@users.noreply.github.com>
2020-11-05 14:56:07 +0000

    Apply fixes from StyleCI

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -3,18 +3,16 @@
 namespace Freddymu\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
-use Freddymu\ChuckNorrisJokes\JokeFactory;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        
     }
 
     public function register()
     {
-        $this->app->bind('chuck-norris', function(){
+        $this->app->bind('chuck-norris', function () {
             return new JokeFactory();
         });
     }
