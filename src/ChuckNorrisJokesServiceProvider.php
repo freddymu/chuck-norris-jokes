392c4955b4e8ab14d30cd7c2a59d78032a150c79
Freddy <fredhopelane@gmail.com>
2020-11-06 13:49:29 +0700

    add config file to customize route name

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -17,13 +17,17 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
             ]);
         }
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chuck-norris');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'chuck-norris');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/chuck-norris'),
-        ]);
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/chuck-norris'),
+        ], 'views');
 
-        Route::get('chuck-norris', ChuckNorrisController::class);
+        $this->publishes([
+            __DIR__ . '/../config/chuck-norris.php' => base_path('config/chuck-norris.php'),
+        ], 'config');
+
+        Route::get(config('chuck-norris.route'), ChuckNorrisController::class);
     }
 
     public function register()
@@ -31,5 +35,7 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
         $this->app->bind('chuck-norris', function () {
             return new JokeFactory();
         });
+
+        $this->mergeConfigFrom(__DIR__ . '/../config/chuck-norris.php', 'chuck-norris');
     }
 }
